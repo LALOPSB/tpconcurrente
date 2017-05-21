ead00e421acacb5119659be184bc765bd28b8cf3
julia <julia@julia-HP-ProBook-4710s>
2017-05-21 20:07:14 -0300

    Para encarar la tarea complicada hice primero un secuencial. El concurrente que hiciste vos no lo toqué, solo lo comenté

--- TPConcurrente/src/Lista.java
+++ TPConcurrente/src/Lista.java
@@ -3,120 +3,165 @@ public class Lista {
 
 	private int[] lista;
 	int last = 0;
-	
-	Lista(int numero){
+
+	Lista(int numero) {
 		lista = new int[numero];
-		
+
 	}
-	
-	public synchronized int size(){
+
+	public synchronized int size() {
 		return last;
 	}
-	
-	public synchronized Boolean isEmpty(){
-		return last==0;
+
+	public synchronized Boolean isEmpty() {
+		return last == 0;
 	}
-	
-	public synchronized Boolean contains(int numero){
-			Boolean existeN = false;
-			for (int i=0; i < last; i++){
-				if (lista[i]==numero) existeN = true;
-			}
-			return existeN;
+
+	public synchronized Boolean contains(int numero) {
+		Boolean existeN = false;
+		for (int i = 0; i < last; i++) {
+			if (lista[i] == numero)
+				existeN = true;
+		}
+		return existeN;
 	}
-	
-	public synchronized void add(int numero){
-		lista[last]=numero;
+
+	public synchronized void add(int numero) {
+		lista[last] = numero;
 		last++;
-		if(last>lista.length-1){
+		if (last > lista.length - 1) {
 			this.duplicateSizeAndCopy();
 		}
 		notifyAll();// para el peek y el pop
 	}
-	
-	public synchronized void addAll(Lista b){
-		for (int i=0;i<b.getLast();i++){
+
+	public synchronized void addAll(Lista b) {
+		for (int i = 0; i < b.getLast(); i++) {
 			this.add(b.getLista()[i]);
 		}
-		
+
 	}
 
 	private synchronized void duplicateSizeAndCopy() {
-		int nuevaLista[] = new int[2*lista.length];
-        for (int i = 0; i < lista.length; i++) {
-            nuevaLista[i] = lista[i];
-        }
-        lista=nuevaLista;
+		int nuevaLista[] = new int[2 * lista.length];
+		for (int i = 0; i < lista.length; i++) {
+			nuevaLista[i] = lista[i];
+		}
+		lista = nuevaLista;
 	}
-	
-	public synchronized int peek(){ // Dijo que si está vacia espera.
-		while (last == 0) { // pongo while porque en monitores no va if para la espera
-		      { try {// esto es sintaxis para que java no chille
-					this.wait(); 
-					}
-			      	catch (InterruptedException e) {}
-			      }
-		}	
+
+	public synchronized int peek() { // Dijo que si está vacia espera
+		while (last == 0) { // pongo while porque en monitores no va if para la
+							// espera
+			{
+				try {// esto es sintaxis para que java no chille
+					this.wait();
+				} catch (InterruptedException e) {
+				}
+			}
+		}
 		return lista[0];
 	}
-	
-	public synchronized int pop(){ // igual a peek 
+
+	public synchronized int pop() { // igual a peek
 		while (last == 0) {
-		      { try {
-					this.wait(); 
-					}
-			      	catch (InterruptedException e) {}
-			      }
+			{
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+				}
+			}
 		}
 		int res = lista[0];
-		for (int i=1;i<last;i++){
-			lista[i-1]=lista[i];
+		for (int i = 1; i < last; i++) {
+			lista[i - 1] = lista[i];
 		}
 		last--;
 		return res;
 	}
-	
-	public synchronized Lista getSublist(int a, int b){
-		Lista sublista = new Lista(b-a);
-		for (int i=a;i<b;i++){
+
+	public synchronized Lista getSublist(int a, int b) {
+		Lista sublista = new Lista(b - a);
+		for (int i = a; i < b; i++) {
 			sublista.add(lista[i]);
 		}
 		return sublista;
-		
+
 	}
-	
-	public Lista mergesort(Lista list, int n) throws InterruptedException{ 
-		if(list.size()<=1)return list;
-		Lista left = list.getSublist(0,list.size()/2);
-		Lista  right = list.getSublist(list.size()/2, list.size());
-		n--;
-		System.out.println(n);
-		if (n==0) notify();
-		User l = new User(left,n);
-		User r = new User(right,n);
-		l.start();
-		r.start();
-		wait();
-		list = merge(left,right);
+
+	public void sortSecuencial() {
+		Lista l = this.mergesortSec(this);
+		int[] a = l.getLista();
+		for (int i = 0; i < l.size(); i++) {
+			lista[i] = a[i];
+		}
+	}
+
+	private synchronized Lista mergesortSec(Lista list) {
+		if (list.size() > 1) {
+			Lista left = list.getSublist(0, list.size() / 2);
+			Lista right = list.getSublist(list.size() / 2, list.size());
+			Lista ll = list.mergesortSec(left);
+			Lista lr = list.mergesortSec(right);
+			list = merge(ll, lr);
+		}
 		return list;
 	}
-	
-	public synchronized Lista merge(Lista left, Lista right) throws InterruptedException{
+
+	public void sort(int n) {   // Ahora hay que agregar threads!!!!
+		Lista l = this.mergesort(this, n);
+		int[] a = l.getLista();
+		for (int i = 0; i < l.size(); i++) {
+			lista[i] = a[i];
+		}
+	}
+
+	private synchronized Lista mergesort(Lista list, int n) { // Ahora hay que agregar threads!!!!
+		if (list.size() > 1) {
+			Lista left = list.getSublist(0, list.size() / 2);
+			Lista right = list.getSublist(list.size() / 2, list.size());
+			Lista ll = list.mergesort(left, n);
+			Lista lr = list.mergesort(right, n);
+			list = merge(ll, lr);
+		}
+		return list;
+	}
+
+	// private synchronized Lista mergesort(Lista list, int n) throws
+	// InterruptedException {
+	// if (list.size() <= 1)!!!!
+	// return list;
+	// Lista left = list.getSublist(0, list.size() / 2);
+	// Lista right = list.getSublist(list.size() / 2, list.size());
+	// n--;
+	// System.out.println(n);
+	// if (n == 0)
+	// notify();
+	// User l = new User(left, n);
+	// User r = new User(right, n);
+	// l.start();
+	// r.start();
+	// wait();
+	// list = merge(left, right);
+	// return list;
+	// }
+
+	private synchronized Lista merge(Lista left, Lista right) {
 		Lista result = new Lista(left.getLast() + right.getLast());
-		
-		while (!left.isEmpty() && !right.isEmpty()){
-			if (left.peek() <= right.peek())result.add(left.pop());
-			else 							result.add(right.pop());
+
+		while (!left.isEmpty() && !right.isEmpty()) {
+			if (left.peek() <= right.peek())
+				result.add(left.pop());
+			else
+				result.add(right.pop());
 		}
 		result.addAll(left);
 		result.addAll(right);
 		return result;
 	}
-	
-	
-	
-//============== getters & setters ============
-	
+
+	// ============== getters & setters ============
+
 	public int[] getLista() {
 		return lista;
 	}
@@ -133,5 +178,4 @@ public class Lista {
 		this.last = last;
 	}
 
-	
 }
